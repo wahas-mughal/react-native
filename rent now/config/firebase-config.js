73f0fea0f20a5037bfd47b10ae6cad9f24222a0a
wahas_mughal <wahasali22@gmail.com>
2021-02-05 02:17:28 +0500

    update

--- rent now/config/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyB1CxkIfZpxlp-_bY8G25MvdDdrEqOWAXQ",
-    authDomain: "rent-a-car-app-211bf.firebaseapp.com",
-    databaseURL: "https://rent-a-car-app-211bf.firebaseio.com",
-    projectId: "rent-a-car-app-211bf",
-    storageBucket: "rent-a-car-app-211bf.appspot.com",
-    messagingSenderId: "343530227842",
-    appId: "1:343530227842:web:075c3ca04fe9f70b14cc9b"
-  };
